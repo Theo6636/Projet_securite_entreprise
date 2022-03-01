d4a2694954ca7c4d47ad8bfd0dcb2488f0bbc371
Theo <theo.ruchot@gmail.com>
2022-03-01 15:51:18 +0100

    bdd

--- securit___entreprise.sql
+++ securit___entreprise.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 13 jan. 2022 à 09:59
+-- Généré le : mar. 01 mars 2022 à 14:50
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -31,7 +31,7 @@ DROP TABLE IF EXISTS `client`;
 CREATE TABLE IF NOT EXISTS `client` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
-  `prénom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
   `adresse` varchar(255) NOT NULL,
   `mail` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
@@ -41,8 +41,8 @@ CREATE TABLE IF NOT EXISTS `client` (
 -- Déchargement des données de la table `client`
 --
 
-INSERT INTO `client` (`id`, `nom`, `prénom`, `adresse`, `mail`) VALUES
-(1, 'Decathlon', 'fabien', '1 rue decathlon', ''),
+INSERT INTO `client` (`id`, `nom`, `prenom`, `adresse`, `mail`) VALUES
+(1, 'Decathlon', 'fabien', '1 rue decathlon', 'fabien@decat.fr'),
 (2, 'tesla', 'elon', '3 rue tesla', '');
 
 -- --------------------------------------------------------
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `commande` (
   `statut` varchar(255) NOT NULL,
   `facturation` tinyint(1) NOT NULL,
   `id_commande` int(11) NOT NULL,
-  `quantité` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL,
   `id_produit` int(11) NOT NULL,
   `id_client` int(11) NOT NULL,
   `id_metier_en_charge` int(11) NOT NULL,
@@ -65,16 +65,25 @@ CREATE TABLE IF NOT EXISTS `commande` (
   KEY `id_produit` (`id_produit`),
   KEY `id_client` (`id_client`),
   KEY `id_metier_en_charge` (`id_metier_en_charge`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`id`, `statut`, `facturation`, `id_commande`, `quantite`, `id_produit`, `id_client`, `id_metier_en_charge`) VALUES
+(20, 'Created', 0, 2, 103, 2, 2, 1),
+(21, 'Created', 0, 3, 104, 1, 1, 1),
+(22, 'Created', 0, 4, 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `métiers`
+-- Structure de la table `metiers`
 --
 
-DROP TABLE IF EXISTS `métiers`;
-CREATE TABLE IF NOT EXISTS `métiers` (
+DROP TABLE IF EXISTS `metiers`;
+CREATE TABLE IF NOT EXISTS `metiers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `mot_de_passe` varchar(255) NOT NULL,
@@ -84,10 +93,10 @@ CREATE TABLE IF NOT EXISTS `métiers` (
 ) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `métiers`
+-- Déchargement des données de la table `metiers`
 --
 
-INSERT INTO `métiers` (`id`, `username`, `mot_de_passe`, `role`, `mail`) VALUES
+INSERT INTO `metiers` (`id`, `username`, `mot_de_passe`, `role`, `mail`) VALUES
 (1, 'commercial', 'commercial', 'commercial', ''),
 (2, 'respoProd', 'respoProd', 'respoProd', ''),
 (3, 'comptable', 'comptable', 'comptable', ''),
@@ -105,7 +114,7 @@ DROP TABLE IF EXISTS `produit`;
 CREATE TABLE IF NOT EXISTS `produit` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
-  `quantité_en_stock` int(11) NOT NULL,
+  `quantite_en_stock` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
@@ -113,8 +122,8 @@ CREATE TABLE IF NOT EXISTS `produit` (
 -- Déchargement des données de la table `produit`
 --
 
-INSERT INTO `produit` (`id`, `nom`, `quantité_en_stock`) VALUES
-(1, 'boite noire', 30),
+INSERT INTO `produit` (`id`, `nom`, `quantite_en_stock`) VALUES
+(1, 'boite noire', 530),
 (2, 'boite blanche', 15);
 COMMIT;
 

030fbf05883b21e27183537a6a5f39bb6b868255
benaigo <benaigo59@yahoo.fr>
2022-03-02 14:10:46 +0100

    stock

--- stock.php
+++ stock.php
@@ -6,22 +6,48 @@
 <body>
 
 <h2>Stock</h2>
-
+<a href="index.php">Retour à l'accueil</a>
 <table border="2">
   <tr>
     <td>Sr.No.</td>
     <td>Nom du produit</td>
     <td>Quantité en stock</td>
-    <td>Mise en production supplementaire</td>
+    <td>mise en production supplementaire</td>
   </tr>
 
 <?php
 
 include "dbConn.php"; // Using database connection file here
-if(isset($_POST['validation']))
+if(isset($_POST['plusBoiteNoire']))
 {   
-    $insert = mysqli_query($db,"UPDATE `produit` SET `quantite_en_stock`= `quantite_en_stock`+'100' WHERE `nom`='boite noire'");
+    $insert = mysqli_query($db,"UPDATE `produit` SET `mis_en_prod`= `mis_en_prod`+'5' WHERE `nom`='boite noire'");
 
+    if(!$insert)
+    {
+        echo mysqli_error($db);
+    }
+    else
+    {
+        echo "Records added successfully.";
+    }
+}
+if(isset($_POST['plusBoiteBlanche']))
+{   
+    $insert = mysqli_query($db,"UPDATE `produit` SET `mis_en_prod`= `mis_en_prod`+'5' WHERE `nom`='boite blanche'");
+
+    if(!$insert)
+    {
+        echo mysqli_error($db);
+    }
+    else
+    {
+        echo "Records added successfully.";
+    }
+}
+if(isset($_POST['confirmer']))
+{   
+    $insert = mysqli_query($db,"UPDATE `produit` SET `quantite_en_stock`= `mis_en_prod`+ `quantite_en_stock`");
+    $insert2 = mysqli_query($db,"UPDATE `produit` SET `mis_en_prod`= '0'");
     if(!$insert)
     {
         echo mysqli_error($db);
@@ -42,18 +68,26 @@ while($data = mysqli_fetch_array($records))
     <td><?php echo $data['id']; ?></td>
     <td><?php echo $data['nom']; ?></td>
     <td><?php echo $data['quantite_en_stock']; ?></td>
+    <td><?php echo $data['mis_en_prod']; ?></td>
     
 
-  </tr>	
+  </tr> 
 <?php
 }
 ?>
 </table>
-<button name="plusBoiteBlanche">+5 boites blanches</button>
-<div>+5 boites noires</div>
+
 <form method="post">
 
-<input type="submit" name="validation" value="Envoyer">
+<input type="submit" name="plusBoiteNoire" value="+5 boites noires">
+</form>
+<form method="post">
+
+<input type="submit" name="plusBoiteBlanche" value="+5 boites blanches">
+</form>
+<form method="post">
+
+<input type="submit" name="confirmer" value="confirmer">
 </form>
 
 <?php mysqli_close($db); // Close connection ?>
